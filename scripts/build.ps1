7ed966ccdc4e23f74d1fab7cd6d777c4419f3b7d
Michal Kelnar <michal@oidis.org>
2025-01-25 18:22:55 +0100

    [OID-85] fixed PowerShell in win runner and fixed docker-compose exit

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,7 @@
 # * ********************************************************************************************************* *
 # *
 # * Copyright 2024 NXP
+# * Copyright 2025 Oidis
 # *
 # * SPDX-License-Identifier: BSD-3-Clause
 # * The BSD-3-Clause license for this file can be found in the LICENSE.txt file included with this distribution
@@ -8,20 +9,27 @@
 # *
 # * ********************************************************************************************************* *
 
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-$projectRoot = [IO.Path]::GetFullPath((Join-Path ".\..\" (get-item $scriptPath ).parent))
-
-if (-Not(Test-Path "$projectRoot\package.json"))
+try
 {
-    Write-Output "Incorrect project root."
-    exit 1
+    Push-Location
+    $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+    $projectRoot = [IO.Path]::GetFullPath((Join-Path ".\..\" (get-item $scriptPath).parent))
+
+    if (-Not (Test-Path "$projectRoot\package.json"))
+    {
+        Write-Output "Incorrect project root."
+        exit 1
+    }
+    Set-Location -Path "$projectRoot\src\builder"
+
+    $env:COMPOSE_MENU= "disable"
+    powershell -NoProfile -Command "docker-compose -f builder.yml up --force-recreate --build --remove-orphans --abort-on-container-failure"
+
+    if ($LastExitCode -ne 0)
+    {
+        Write-Error "docker build failed"
+        exit 1
+    }
+} finally {
+    Pop-Location
 }
-cd -Path "$projectRoot\src\builder"
-
-docker-compose -f builder.yml up --force-recreate --build --remove-orphans --abort-on-container-exit
-
-if ($LastExitCode -ne 0) {
-    exit 1
-}
-
-cd -Path "$scriptPath"
